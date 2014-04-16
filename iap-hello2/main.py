230390b27f98345a1c7af30c214fd08cce2925de
Matt Lebrun <lebrun.matt@gmail.com>
2014-04-16 10:58:24 +0800

    Renamed applications

---
+++ iap-hello2/main.py
@@ -0,0 +1,14 @@
+import kivy
+kivy.require('1.8.0')
+
+from kivy.app import App
+from kivy.uix.button import Label
+
+
+class Hello2App(App):
+    def build(self):
+        return Label()
+
+
+if __name__ == '__main__':
+    Hello2App().run()
---
+++ iap-widgets/main.py
@@ -0,0 +1,18 @@
+import kivy
+kivy.require('1.8.0')
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MyWidget(Widget):
+    pass
+
+
+class WidgetsApp(App):
+    def build(self):
+        return MyWidget()
+
+
+if __name__ == '__main__':
+    WidgetsApp().run()
